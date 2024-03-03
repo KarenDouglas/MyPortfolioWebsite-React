cafb07d4852bbc700814b57c73d830c0ac5b3fc6
Karen Douglas <kdindependence@gmail.com>
2024-03-02 20:19:50 -0600

    set up basic file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       APP
+      </div>
+      
+    </>
+  )
+}
+
+export default App
