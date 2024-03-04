dcba3979eacbb798d3c10f5cc4620612964c531c
Karen Douglas <kdindependence@gmail.com>
2024-03-04 07:09:32 -0600

    add form validation

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -3,19 +3,29 @@ import '../css/form.css'
 
 
 export default function Form() {
+    const handleBlur = (e) => {
+        const inputValue = e.target.value
+        if(inputValue.trim() === ''){
+          e.target.style.border = 'red 5px solid'
+        }else{
+            e.target.style.border = 'green 5px solid'
+        }
+    }
+
+
     return (
         <>
     {/* form */}
     <section className='form-container'>
         <form className='form' action="">
             <div className='form-group1'>
-            <input  type="text" placeholder='enter name...' required />
-            <input type="email" placeholder='enter email...' required />
+            <input  type="text" placeholder='enter name...' onBlur={handleBlur} required />
+            <input type="email" placeholder='enter email...' onBlur={handleBlur} required />
             </div>
            
             <div className='form-group'>
-            <textarea name="message" id="" placeholder='enter your message for Karen' cols="30" rows="10" > </textarea>
-            <button>Submit</button>
+            <textarea name="message" id=""  cols="30" rows="10" defaultValue="enter message"></textarea>
+            <button type='submit'>Submit</button>
             </div>
         </form>
     </section>
