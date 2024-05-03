6d044ea782dc25be50b99339396bd299331f345b
Karen Douglas <kdindependence@gmail.com>
2024-05-03 15:11:00 -0500

    set up basic smtp server for emails

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 
 app.use(express.urlencoded({ extended: false }))
@@ -17,15 +18,51 @@ const corsOptions = {
 // uses cors options on all routes
 app.use(cors(corsOptions));
 
+// Create a transporter object using SMTP transport
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, 
+  auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+  }
+});
+
+
 //get 
 app.get('/',  (req, res) => {
     res.json('Hello World!')
 })
 
+
+
+// Setup email data
+let mailOptions = {
+    from: `"Karen Douglas" <${process.env.SMTP_USER}>`, 
+    to: `${process.env.SMTP_USER}`,
+    subject: 'test ✔',
+    text: 'Hello world?', 
+    html: '<b>Hello world?</b>' 
+}
+
+// Send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+});
+
 //post 
-app.post('/sendEmail', (req, res) =>{
-    console.log(req.body)
-    res.json(req.body)
+let postCount = 0;
+app.post('/sendEmail', async(req, res) =>{
+  postCount++
+  // ensures that I only recieve one post request per fetch
+    if(postCount > 1){
+      postCount = 0
+      return
+    } 
 })
 
 app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT}`));
