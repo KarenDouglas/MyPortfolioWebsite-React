9153c22e8d6ff65ae9e233f56cf66df20b34c998
Karen Douglas <kdindependence@gmail.com>
2024-05-03 17:33:22 -0500

    created functions to send confirmation email and message to myself

--- server/server.js
+++ server/server.js
@@ -1,33 +1,24 @@
 const express = require('express')
 const cors = require('cors')
-const nodemailer = require('nodemailer')
+
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 3001
-
+const { sendEmailConfirmation, sendEmailMessage} = require('./utils')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // middleware to set CORS headers for specific routes
 const corsOptions = {
-  origin: ['http://127.0.0.1:5173', 'https://karenportfolio.netlify.app'], // enabling specific domains
+  origin: ['http://127.0.0.1:5173', 'https://karenportfolio.netlify.app','https://karenthedeveloper.com/'], // enabling specific domains
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }
 // uses cors options on all routes
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-// Create a transporter object using SMTP transport
-let transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, 
-  auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-  }
-});
+
 
 
 //get 
@@ -37,22 +28,7 @@ app.get('/',  (req, res) => {
 
 
 
-// Setup email data
-let mailOptions = {
-    from: `"Karen Douglas" <${process.env.SMTP_USER}>`, 
-    to: `${process.env.SMTP_USER}`,
-    subject: 'test ✔',
-    text: 'Hello world?', 
-    html: '<b>Hello world?</b>' 
-}
 
-// Send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-});
 
 //post 
 let postCount = 0;
@@ -62,7 +38,9 @@ app.post('/sendEmail', async(req, res) =>{
     if(postCount > 1){
       postCount = 0
       return
-    } 
+    }
+    sendEmailConfirmation('Karen Douglas', '_')
+    sendEmailMessage('Grantie', 'fake@email.com', 'This is my thoughtful message') 
 })
 
 app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT}`));
