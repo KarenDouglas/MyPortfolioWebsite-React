736980e139b6cb4ab923fc07594b0cc8bf2b0862
Karen Douglas <kdindependence@gmail.com>
2024-05-05 16:07:54 -0500

    fixed spacing on about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -13,7 +13,7 @@ export default  function AboutPage() {
             </picture>
             <section className='pitch'>
                 <h1>Transforming Ideas into Dynamic Web Experiences</h1>
-                <p className="desc">As a passionate MERN stack developer, my expertise lies in crafting dynamic web applications with a focus on React and API integration. Driven by a desire to make a difference and continuously sharpen my skills, I thrive in environments where innovation and collaboration flourish. I am deeply committed to lifelong learning and find immense joy in witnessing my ideas materialize into functional and impactful web solutions. Eager to contribute my talents to a team where creativity meets purpose, I am dedicated to driving forward projects that resonate with users and drive positive change!</p>
+                <p className="desc">As a passionate MERN stack developer, my expertise lies in crafting dynamic web applications with a focus on React and API integration. Driven by a desire to make a difference and continuously sharpen my skills, I thrive in environments where innovation and collaboration flourish. </p>
                 <p><a className="hire" href="/contact">Hire Me</a></p>
             </section>
         </main>
